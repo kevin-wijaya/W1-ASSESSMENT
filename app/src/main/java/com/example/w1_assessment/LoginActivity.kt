cc77fe658df0cd2254269fea352e01cfae22fb96
kevin-wijaya <yohaneskevinwijaya@gmail.com>
2023-03-02 19:19:46 +0700

    Buat Menu Login - belum jadi

---
+++ app/src/main/java/com/example/w1_assessment/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.w1_assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    //Buat Menu Login
+}
