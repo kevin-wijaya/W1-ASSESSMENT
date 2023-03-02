9cc4766ebbbdca0c3ae49a2cf4ba68b29df1db67
kevin-wijaya <yohaneskevinwijaya@gmail.com>
2023-03-02 19:43:39 +0700

    Fitur Register - fitur  captcha

--- app/src/main/java/com/example/w1_assessment/LoginActivity.kt
+++ app/src/main/java/com/example/w1_assessment/LoginActivity.kt
@@ -2,11 +2,14 @@ package com.example.w1_assessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Toast.makeText(this, "ini fitur login cuy", Toast.LENGTH_SHORT).show()
     }
 
     //Buat Menu Login
--- app/src/main/java/com/example/w1_assessment/RegisterActivity.kt
+++ app/src/main/java/com/example/w1_assessment/RegisterActivity.kt
@@ -6,8 +6,9 @@ import android.os.Bundle
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout .activity_register)
     }
 
     // Ini fitur register
+
 }
