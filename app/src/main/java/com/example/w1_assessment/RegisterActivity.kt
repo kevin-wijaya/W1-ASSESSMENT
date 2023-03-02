c612bd90554266e4028f3216b4af3d58158af50e
hendyardhana <hendy.ardha1708@gmail.com>
2023-03-02 19:29:08 +0700

    Fitur Register selesai

---
+++ app/src/main/java/com/example/w1_assessment/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.example.w1_assessment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    // Ini fitur register
+}
